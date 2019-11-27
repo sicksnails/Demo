b0b8411e2498324dfd918486050c293394287e35
snail <weiwuduhan@163.com>
2019-11-27 19:02:27 +0800

    代码实现缩印pdf

--- timell.py
+++ timell.py
@@ -33,23 +33,40 @@ def merge(pdfFile, outfile, mode, start=0):  # 输入，输出，几页合一的
 
                 n = math.sqrt(mode / 2) # 计算布局短边
 
-                txs = int((n - 1 - int(i / (2 * n))) * w / n)
-                tys = int(int((2 * n - 1 - i % (2 * n))) * h / (2 * n) + 215)
-                tys = tys if tys > 0 else 0
-
-                print(tys)
-
                 if w < h:
+                    txs = int((n - 1 - int(i / (2 * n))) * w / n)
+                    tys = int(int((2 * n - 1 - i % (2 * n))) * h / (2 * n) + 215)
+                    tys = tys if tys > 0 else 0
                     newpage.mergeRotatedScaledTranslatedPage(page, -90, 1 / scale_para, txs, tys)  # up offset
+                else:
+                    txs = int((2 * n - 1 - int(i / n)) * w / (2 * n))
+                    tys = int((n - 1 - i % n)) * h / n
+                    tys = tys if tys > 0 else 0
+                    newpage.mergeRotatedScaledTranslatedPage(page, -90, 1 / scale_para,txs,)
+                    newpage.rotateClockwise(0)
 
-                newpage.rotateClockwise(0)
-            newpdf.addPage(newpage)
+        elif mode == 4 or mode == 9 or mode == 16:
+            for i in range(mode):
+                if mode * j + i >= total:  # 边界判定
+                    break
+                page = pdfobj.getPage(start + mode * j + i)  # 获取需要处理的page对象
+                (w, h) = page.mediaBox.upperRight  # 获取宽高
+                w = int(w)  # 对象转int
+                h = int(h)
+                scale_para = int(math.sqrt(mode))  # 计算缩放系数
+                newpage.mergeRotatedScaledTranslatedPage(page, 0, 1 / scale_para,
+                                                         # scale while 4 -> 1/sqrt(4)=0.5
+                                                         int((i % scale_para) * w / scale_para),
+                                                         # right offset
+                                                         int((scale_para - 1 - int(
+                                                             i / scale_para)) * h / scale_para))  # up offset
+        newpdf.addPage(newpage)
     newpdf.write(open(outfile, 'wb'))
 
 
 if __name__ == '__main__':
     rootPath = 'E:\\测试文件\\demo\\'  # 文件夹根目录
-    inPdf = 'https://cloud-printing.oss-cn-beijing.aliyuncs.com/10001/applet/document/init/print_201911271824466513d7308-pdf/print_201911271824466513d7308.pdf'
+    inPdf = rootPath + '转换的PDF.pdf'
     outPdf = rootPath + 'ossPdf.pdf'
 
     merge(inPdf, outPdf, 8)
